47c958ea3e0915e373b2cb058d69ac0ba2292c2d
Ali Al-Dhib <alifahed67@gmail.com>
2022-08-05 13:35:33 +0800

    Add placeMarker function

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const Gameboard = (() => {
-  const gameboard = ['0', 'O', 'X', 'O', 'X', 'O', 'X', 'O', '8'];
+  // const gameboard = ['0', 'O', 'X', 'O', 'X', 'O', 'X', 'O', '8'];
+  const gameboard = [];
 
   return { gameboard };
 })();
@@ -28,12 +29,60 @@ const displayController = (() => {
       gamebox7.textContent = Gameboard.gameboard[7];
       gamebox8.textContent = Gameboard.gameboard[8];
     }
+    return { gamebox0, gamebox1, gamebox2, gamebox3, gamebox4, gamebox5, gamebox6, gamebox7, gamebox8 };
   };
   return { renderArrayToWebpage };
 })();
 
 const Players = () => {
+  const placeMarker = () => {
+    displayController.renderArrayToWebpage().gamebox0.addEventListener('click', () => {
+      Gameboard.gameboard[0] = 'X';
+      displayController.renderArrayToWebpage().gamebox0.textContent = 'X';
+    });
 
+    displayController.renderArrayToWebpage().gamebox1.addEventListener('click', () => {
+      Gameboard.gameboard[1] = marker;
+      displayController.renderArrayToWebpage().gamebox1.textContent = marker;
+    });
+    
+    displayController.renderArrayToWebpage().gamebox2.addEventListener('click', () => {
+      Gameboard.gameboard[2] = marker;
+      displayController.renderArrayToWebpage().gamebox2.textContent = marker;
+    });
+
+    displayController.renderArrayToWebpage().gamebox3.addEventListener('click', () => {
+      Gameboard.gameboard[3] = marker;
+      displayController.renderArrayToWebpage().gamebox3.textContent = marker;
+    });
+
+    displayController.renderArrayToWebpage().gamebox4.addEventListener('click', () => {
+      Gameboard.gameboard[4] = marker;
+      displayController.renderArrayToWebpage().gamebox4.textContent = marker;
+    });
+
+    displayController.renderArrayToWebpage().gamebox5.addEventListener('click', () => {
+      Gameboard.gameboard[5] = marker;
+      displayController.renderArrayToWebpage().gamebox5.textContent = marker;
+    });
+
+    displayController.renderArrayToWebpage().gamebox6.addEventListener('click', () => {
+      Gameboard.gameboard[6] = marker;
+      displayController.renderArrayToWebpage().gamebox6.textContent = marker;
+    });
+
+    displayController.renderArrayToWebpage().gamebox7.addEventListener('click', () => {
+      Gameboard.gameboard[7] = marker;
+      displayController.renderArrayToWebpage().gamebox7.textContent = marker;
+    });
+
+    displayController.renderArrayToWebpage().gamebox8.addEventListener('click', () => {
+      Gameboard.gameboard[8] = marker;
+      displayController.renderArrayToWebpage().gamebox8.textContent = marker;
+    });
+  };
+  return { placeMarker };
 };
 
 displayController.renderArrayToWebpage();
+Players().placeMarker();
